89b8c9e2e51792050a13bbe5dd6d3cefa9928241
Paul Sonnentag <paul.sonnentag@googlemail.com>
2023-07-03 14:50:14 +0200

    Add code editor

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 // And henceforth, we can load React! JSX still TBD.
 const React = await import("react")
-const { useState, useCallback, useEffect } = React
+const { useState, useCallback, useEffect, useRef } = React
 const client = await import("react-dom/client")
 const { useDocument, RepoContext } = await import("@automerge/automerge-repo-react-hooks")
 const { html } = await import("htm/react")
 //const repo = await import("repo")
 const { createRoot } = client
 const ReactJson = await import("react-json-view")
+const { EditorView, keymap } = await import("@codemirror/view")
+const { basicSetup } = await import("codemirror")
+const { javascript } = await import("@codemirror/lang-javascript")
+const { indentWithTab } = await import("@codemirror/commands")
 
 // Clear the existing HTML content
 document.body.innerHTML = '<div id="app"></div>'
@@ -19,11 +23,22 @@ root.render(
 
 function Root() {
   const documentId = new URL(location.href).searchParams.get("documentId")
+  const [doc] = useDocument(documentId)
+  const [isCodeEditorSelected, setIsCodeEditorSelected] = useState(false)
+
+  if (!doc) {
+    return null
+  }
 
   return html`
     <div>
-      <h1>Hello react!</h1>
-      <${RawView} documentId=${documentId} />
+      <div>
+        <button onClick=${() => setIsCodeEditorSelected(false)}>data</button>
+        ${doc.source && html`<button onClick=${() => setIsCodeEditorSelected(true)}>code</button>`}
+      </div>
+      ${isCodeEditorSelected
+        ? React.createElement(CodeEditor, { documentId })
+        : React.createElement(RawView, { documentId })}
     </div>
   `
 }
@@ -111,4 +126,52 @@ function RawView(props) {
     onDelete,
     onSelect,
   })
+}
+
+function CodeEditor({ documentId }) {
+  const [doc, changeDoc] = useDocument(documentId)
+
+  const onChangeSource = useCallback(
+    (newSource) => {
+      changeDoc((doc) => (doc.source = newSource))
+    },
+    [changeDoc]
+  )
+
+  if (!doc) {
+    return null
+  }
+
+  return React.createElement(CodeMirror, { source: doc.source, onChangeSource })
+}
+
+function CodeMirror({ source, onChangeSource }) {
+  const [container, setContainer] = useState(null)
+  const editorViewRef = useRef()
+
+  useEffect(() => {
+    if (!container) {
+      return
+    }
+
+    const view = (editorViewRef.current = new EditorView({
+      doc: source,
+      extensions: [basicSetup, javascript({ jsx: true }), keymap.of([indentWithTab])],
+      dispatch(transaction) {
+        view.update([transaction])
+
+        if (transaction.docChanged) {
+          const newValue = view.state.doc.toString()
+          onChangeSource(newValue)
+        }
+      },
+      parent: container,
+    }))
+
+    return () => {
+      view.destroy()
+    }
+  }, [container])
+
+  return React.createElement("div", { ref: setContainer })
 }
