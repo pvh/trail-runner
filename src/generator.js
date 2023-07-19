85f9654adb4e911553b106eb650e1778994d151b
Peter van Hardenberg <pvh@pvh.ca>
2023-07-19 11:56:06 -0700

    remove generator -- looks like we can use stock again

--- src/generator.js
+++
@@ -1,7 +0,0 @@
-export { G as Generator, d as analyzeHtml, e as clearCache, f as fetch, i as getDefaultProviders, h as getPackageBase, g as getPackageConfig, l as lookup, p as parseUrlPkg } from './generator-b4f53f2d.js';
-import '#fetch';
-import '@jspm/import-map';
-import 'process';
-import 'sver';
-import 'sver/convert-range.js';
-import 'es-module-lexer/js';
