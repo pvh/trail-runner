46266503127fc76077d82dcbf2f16ce7a08ebf62
Paul Sonnentag <paul.sonnentag@googlemail.com>
2023-08-16 14:55:05 +0200

    Allow to bootstrap any module by passing the documentId as url param

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ import { IndexedDBStorageAdapter } from "@automerge/automerge-repo-storage-index
 import { BrowserWebSocketClientAdapter } from "@automerge/automerge-repo-network-websocket"
 import { AutomergeRegistry } from "./automerge-provider.js"
 
-const PRECOOKED_BOOTSTRAP_DOC_ID = "441f8ea5-c86f-49a7-87f9-9cc60225e15e"
 const PRECOOKED_REGISTRY_DOC_ID = "6b9ae2f8-0629-49d1-a103-f7d4ae2a31e0"
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+// const BOOTSTRAP_DOC_ID = "441f8ea5-c86f-49a7-87f9-9cc60225e15e"
+const BOOTSTRAP_DOC_ID = urlParams.get("bootstrapDocId") ?? "cf4edf99-2fa3-4c05-b87d-7e079dc39375"
 
 // Step one: Set up an automerge-repo.
 const repo = new Repo({
@@ -25,14 +30,13 @@ function bootstrap(key, initialDocumentFn) {
     localStorage.setItem(key, handle.documentId)
     return handle
   } else {
-    const handle = repo.find(docId)
-    return handle
+    return repo.find(docId)
   }
 }
 
 // you can BYO but we'll provide a default
 const registryDocHandle = bootstrap("registryKey", (doc) => repo.find(PRECOOKED_REGISTRY_DOC_ID))
-const bootstrapDocHandle = bootstrap("bootstrapKey", (doc) => repo.find(PRECOOKED_BOOTSTRAP_DOC_ID))
+const bootstrapDocHandle = repo.find(BOOTSTRAP_DOC_ID) // bootstrap("bootstrapKey", (doc) => repo.find(PRECOOKED_BOOTSTRAP_DOC_ID))
 
 // temporary hack to make the bootstrap doc available to the existing code
 window.BOOTSTRAP_DOC_ID = bootstrapDocHandle.documentId
@@ -41,6 +45,14 @@ await registryDocHandle.value()
 // We stash the registry on the window so we can access it from the debugger for convenience and hackery
 const registry = (window.registry = new AutomergeRegistry(repo, registryDocHandle))
 registry.installFetch()
+
+// todo: allow to bootstrap documents that are not in the registry
+const names = await registry.findLinkedNames(BOOTSTRAP_DOC_ID)
+if (names.length === 0) {
+  throw new Error("Can't bootstrap document because it has no entry in the package registry")
+}
+
+const bootstrapPackageName = names[0].name
 
 window.esmsInitOptions = {
   shimMode: true,
@@ -72,9 +84,8 @@ await import("https://ga.jspm.io/npm:es-module-shims@1.8.0/dist/es-module-shims.
 // registry has the dependencies for the bootstrap program.
 
 // This code imports the bootstrap document and its dependencies to the registry
-const packageName = "@trail-runner/bootstrap"
-registry.linkPackage(packageName, "0.0.1", `${PRECOOKED_BOOTSTRAP_DOC_ID}`)
-await registry.update(packageName)
+registry.linkPackage(bootstrapPackageName, "0.0.1", `${BOOTSTRAP_DOC_ID}`)
+await registry.update(bootstrapPackageName)
 
 // This code creates a reusable importMap based on the current registry you have
 // and stores it in the bootstrap document.
@@ -85,7 +96,10 @@ const generator = (window.generator = new Generator({
     automerge: registry.jspmProvider(),
   },
 }))
-await generator.install(packageName) // this should load the package above
+
+console.log("load", bootstrapPackageName)
+
+await generator.install(bootstrapPackageName) // this should load the package above
 bootstrapDocHandle.change((doc) => {
   doc.importMap = generator.getMap()
 })
@@ -100,4 +114,8 @@ console.log("Bootstrapping...")
 // registry.updateImportMap("@trail-runner/bootstrap")
 // (does the below) we could maybe do this in the importShim??? or in fetch?
 importShim.addImportMap(importMap)
-const Bootstrap = await import("@trail-runner/bootstrap")
+const rootModule = await import(bootstrapPackageName)
+
+if (rootModule.mount) {
+  rootModule.mount(document.getElementById("root"), { documentId: BOOTSTRAP_DOC_ID })
+}
