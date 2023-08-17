a18e9e1e23d5a0eeb63cdddda79da582afbc7407
Paul Sonnentag <paul.sonnentag@googlemail.com>
2023-08-17 08:01:00 +0200

    Pass all url params to mount function

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 import { Generator } from "@jspm/generator"
 import { Repo } from "@automerge/automerge-repo"
 import * as Automerge from "@automerge/automerge"
-import { IndexedDBStorageAdapter } from "@automerge/automerge-repo-storage-indexeddb"
-import { BrowserWebSocketClientAdapter } from "@automerge/automerge-repo-network-websocket"
-import { AutomergeRegistry } from "./automerge-provider.js"
+import {IndexedDBStorageAdapter} from "@automerge/automerge-repo-storage-indexeddb"
+import {BrowserWebSocketClientAdapter} from "@automerge/automerge-repo-network-websocket"
+import {AutomergeRegistry} from "./automerge-provider.js"
 
 const PRECOOKED_REGISTRY_DOC_ID = "6b9ae2f8-0629-49d1-a103-f7d4ae2a31e0"
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
-// const BOOTSTRAP_DOC_ID = "441f8ea5-c86f-49a7-87f9-9cc60225e15e"
-const BOOTSTRAP_DOC_ID = urlParams.get("bootstrapDocId") ?? "cf4edf99-2fa3-4c05-b87d-7e079dc39375"
+const BOOTSTRAP_DOC_ID = urlParams.get("bootstrapDocId") ?? "441f8ea5-c86f-49a7-87f9-9cc60225e15e"
 
 // Step one: Set up an automerge-repo.
 const repo = new Repo({
@@ -36,7 +35,7 @@ function bootstrap(key, initialDocumentFn) {
 
 // you can BYO but we'll provide a default
 const registryDocHandle = bootstrap("registryKey", (doc) => repo.find(PRECOOKED_REGISTRY_DOC_ID))
-const bootstrapDocHandle = repo.find(BOOTSTRAP_DOC_ID) // bootstrap("bootstrapKey", (doc) => repo.find(PRECOOKED_BOOTSTRAP_DOC_ID))
+const bootstrapDocHandle = repo.find(BOOTSTRAP_DOC_ID)
 
 // temporary hack to make the bootstrap doc available to the existing code
 window.BOOTSTRAP_DOC_ID = bootstrapDocHandle.documentId
@@ -65,7 +64,7 @@ if (!window.process) {
   window.process = {}
 }
 console.log("window.process, before: ", window.process)
-window.process.env = { DEBUG_COLORS: "false" }
+window.process.env = {DEBUG_COLORS: "false"}
 window.process.browser = true
 window.process.versions = {}
 window.process.stderr = {}
@@ -80,16 +79,16 @@ await import("https://ga.jspm.io/npm:es-module-shims@1.8.0/dist/es-module-shims.
  * eventually, this should move into the module editor. Feel free to do so!
  * /
 
-// To bootstrap the system from scratch, we need to make sure our initial
-// registry has the dependencies for the bootstrap program.
+ // To bootstrap the system from scratch, we need to make sure our initial
+ // registry has the dependencies for the bootstrap program.
 
-// This code imports the bootstrap document and its dependencies to the registry
-registry.linkPackage(bootstrapPackageName, "0.0.1", `${BOOTSTRAP_DOC_ID}`)
-await registry.update(bootstrapPackageName)
+ // This code imports the bootstrap document and its dependencies to the registry
+ registry.linkPackage(bootstrapPackageName, "0.0.1", `${BOOTSTRAP_DOC_ID}`)
+ await registry.update(bootstrapPackageName)
 
-// This code creates a reusable importMap based on the current registry you have
-// and stores it in the bootstrap document.
-const generator = (window.generator = new Generator({
+ // This code creates a reusable importMap based on the current registry you have
+ // and stores it in the bootstrap document.
+ const generator = (window.generator = new Generator({
   resolutions: { "@automerge/automerge-wasm": "./web/" },
   defaultProvider: "automerge",
   customProviders: {
@@ -97,13 +96,13 @@ const generator = (window.generator = new Generator({
   },
 }))
 
-console.log("load", bootstrapPackageName)
+ console.log("load", bootstrapPackageName)
 
-await generator.install(bootstrapPackageName) // this should load the package above
-bootstrapDocHandle.change((doc) => {
+ await generator.install(bootstrapPackageName) // this should load the package above
+ bootstrapDocHandle.change((doc) => {
   doc.importMap = generator.getMap()
 })
-/**/
+ /**/
 
 const importMap = (await bootstrapDocHandle.value()).importMap
 if (!importMap) {
@@ -117,5 +116,6 @@ importShim.addImportMap(importMap)
 const rootModule = await import(bootstrapPackageName)
 
 if (rootModule.mount) {
-  rootModule.mount(document.getElementById("root"), { documentId: BOOTSTRAP_DOC_ID })
+  const params = Object.fromEntries(urlParams.get())
+  rootModule.mount(document.getElementById("root"), params)
 }
