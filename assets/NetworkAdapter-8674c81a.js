d67db79ec730cfad9f10d31f4f1e633a1965fa1b
Peter van Hardenberg <pvh@pvh.ca>
2023-09-28 01:31:04 -0700

    vite refresh

---
+++ assets/NetworkAdapter-8674c81a.js
@@ -0,0 +1,1 @@
+import{E as e}from"./CollectionSynchronizer-c51cb66c.js";class s extends e{peerId}export{s as N};
--- assets/NetworkAdapter-b7a01c72.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./CollectionSynchronizer-d734ae39.js";class s extends e{peerId}export{s as N};
