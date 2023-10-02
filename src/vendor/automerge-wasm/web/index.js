158800714e2f0f4fb1542c3bb92741656649334b
Peter van Hardenberg <pvh@pvh.ca>
2023-10-02 13:51:46 -0700

    more packaging nonsense

--- src/vendor/automerge-wasm/web/index.js
+++ src/vendor/automerge-wasm/web/index.js
@@ -1,8 +1,8 @@
-import url from "./automerge_wasm_bg.wasm?url";
+//import url from "./automerge_wasm_bg.wasm";
 import { initSync } from "./automerge_wasm.js";
 
 export const promise = new Promise(resolve => {
-  fetch(url).then( (response) => {
+  fetch("./automerge_wasm_bg.wasm").then( (response) => {
     response.arrayBuffer().then(b => {
       initSync(b)
       console.log("sunc")
