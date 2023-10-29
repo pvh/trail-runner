5515b9d45bcb1ff8172b6418c6ea7c0f9a4fbe49
Peter van Hardenberg <pvh@pvh.ca>
2023-10-29 06:40:13 +0000

    bump cache

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ import { IndexedDBStorageAdapter } from "@automerge/automerge-repo-storage-index
 import { BrowserWebSocketClientAdapter } from "@automerge/automerge-repo-network-websocket"
 import { MessageChannelNetworkAdapter } from "@automerge/automerge-repo-network-messagechannel"
 
-const CACHE_NAME = "v1"
+const CACHE_NAME = "v2"
 const ASSETS_TO_CACHE = [
   "/",
   "/automerge_wasm_bg.wasm",
